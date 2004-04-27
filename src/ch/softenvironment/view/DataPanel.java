a92dc31df0e13f24bebdad41d282ab17cfd491c6
phirzel <phirzel@users.noreply.github.com>
2004-04-27 18:49:45 +0000

    Changed from Class to Interface

--- src/ch/softenvironment/view/DataPanel.java
+++ src/ch/softenvironment/view/DataPanel.java
@@ -1,7 +1,5 @@
 package ch.softenvironment.view;
 
-import ch.softenvironment.util.ResourceManager;
-
 /* 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -18,52 +16,15 @@ import ch.softenvironment.util.ResourceManager;
  * Simple Panel to represent a certain Object which
  * allows changes to be saved.
  * @author: Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.3 $ $Date: 2004-04-27 09:14:58 $
+ * @version $Revision: 1.4 $ $Date: 2004-04-27 18:49:45 $
  */
-public abstract class DataPanel extends javax.swing.JPanel {
-/**
- * DataPanel constructor comment.
- */
-public DataPanel() {
-	super();
-}
-/**
- * Adapt the given PopupMenu before displaying it (for e.g. disable Items).
- * Overwrite this method.
- * @see #genericPopupDisplay()
- */
-protected javax.swing.JPopupMenu adaptPopupMenu(javax.swing.JPopupMenu popupMenu) {
-	// overwrite
-	return null;
-}
-/**
- * @see BaseFrame
- */
-protected void genericPopupDisplay(java.awt.event.MouseEvent event, javax.swing.JPopupMenu popupMenu) {
-	if (event.isPopupTrigger()) {
-		adaptPopupMenu(popupMenu);
-		popupMenu.show(event.getComponent(), event.getX(), event.getY());
-	}
-}
+public interface DataPanel {
 /**
  * Return the changed object displayed.
  */
-public abstract Object getObject();
-/**
- * @see BaseFrame#getResourceString(String)
- */
-protected String getResourceString(String propertyName) {
-	return ResourceManager.getInstance().getResource(this.getClass(), propertyName);
-}
-/**
- * Popup an error Dialog.
- * @param exception java.lang.Throwable
- */
-protected void handleException(java.lang.Throwable exception) {
-	new ErrorDialog(this, null, exception.toString(), exception); //$NON-NLS-1$
-}
+java.lang.Object getObject();
 /**
  * Set the Object to be displayed by panel.
  */
-public abstract void setObject(Object object);
+void setObject(java.lang.Object object);
 }
