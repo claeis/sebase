5de0c5b79773473045082d392ca2c590096cce67
phirzel <phirzel@users.noreply.github.com>
2004-05-09 17:22:22 +0000

    Cosmetics

--- src/ch/softenvironment/view/DetailView.java
+++ src/ch/softenvironment/view/DetailView.java
@@ -16,32 +16,25 @@ package ch.softenvironment.view;
  * Method-Set for a DetailView
  * @see BaseFrame (as a Parent-Class)
  * @author: Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.2 $ $Date: 2004-02-05 11:32:59 $
+ * @version $Revision: 1.3 $ $Date: 2004-05-09 17:22:22 $
  */
 public interface DetailView {
-/**
- * Assign a set of aggregates.
- */
-void assignObjects(java.util.List objects);
-/**
- * Do anything at DoubleClick-Event.
- * @return boolean true if event was consumed
- * @see BaseFrame#generatePopupDisplay(..)
- */
-boolean defaultDoubleClickAction(java.awt.event.MouseEvent event);
-/**
- * Save an Object represented by DetailView.
- * @see for e.g. DbEntityBean#save()
- */
-void saveObject();
-/**
- * Make the View represent the given Object.
- * @see Toolbar#getNext()
- */
-void setCurrentObject(Object currentObject);
-/**
- * Undo the changes of an Object represented by this GUI.
- * @see for e.g. DbEntityBean#refresh()
- */
-void undoObject();
+	/**
+	 * Assign a set of aggregates given in objects.
+	 * @param objects
+	 */
+	void assignObjects(java.util.List objects);
+	/**
+	 * Save an Object represented by DetailView.
+	 */
+	void saveObject();
+	/**
+	 * Make the View represent the given Object.
+	 * @param currentObject
+	 */
+	void setCurrentObject(Object object);
+	/**
+	 * Undo the changes of an Object represented by this GUI.
+	 */
+	void undoObject();
 }
