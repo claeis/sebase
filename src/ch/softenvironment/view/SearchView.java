0b10e2b5b442df6dbfc6efb70cd8d735f8dc56c3
phirzel <phirzel@users.noreply.github.com>
2004-05-09 17:26:26 +0000

    Redesigned

--- src/ch/softenvironment/view/SearchView.java
+++ src/ch/softenvironment/view/SearchView.java
@@ -16,32 +16,15 @@ package ch.softenvironment.view;
  * Method-Set for a SearchView
  * @see ApplicationFrame (as a Parent-Class)
  * @author: Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.2 $ $Date: 2004-02-05 11:33:00 $
+ * @version $Revision: 1.3 $ $Date: 2004-05-09 17:26:26 $
  */
-public interface SearchView {
+public interface SearchView extends ListMenuChoice {
 /**
- * Assign the selected Objects in a SearchTable.
+ * Assign the selected Objects in a SearchTable to the caller.
  * @see DbTableModel
  * @exception Throwable Handled by this GUI-Method.
  */
 public void assignObjects();
-/**
- * Create a new Object and open it in a DetailView.
- * @exception Throwable Handled by this GUI-Method.
- */
-public void newObject();
-/**
- * Open the selected Objects.
- * @see DbTableModel
- * @exception Throwable Handled by this GUI-Method.
- */
-public void openObjects();
-/**
- * Remove the selected Object's (for e.g. DbSessionBeans) from a SearchTable.
- * @see DbTableModel remove()
- * @exception Throwable Handled by this GUI-Method.
- */
-void removeObjects();
 /**
  * A SearchView usually offers a set of Query-Fields to
  * make the searching of objects more accurate and performant.
