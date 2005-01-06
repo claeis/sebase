ff92f29d6b0ff59c6c8b392a3859d776c70e67b3
phirzel <phirzel@users.noreply.github.com>
2005-01-06 09:53:07 +0000

    NEW: Support for JTable Columns

---
+++ src/ch/softenvironment/view/table/PercentTableCellRenderer.java
@@ -0,0 +1,45 @@
+package ch.softenvironment.view.table;
+
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * Format a Number value as Amount-String in a JTable-Cell.
+ * @author: Peter Hirzel <i>soft</i>Environment
+ */
+public class PercentTableCellRenderer extends javax.swing.table.DefaultTableCellRenderer {
+	java.text.NumberFormat percentFormat = null;
+public PercentTableCellRenderer() {
+	super();
+	percentFormat = java.text.NumberFormat.getNumberInstance();
+	//				java.text.NumberFormat.getPercentInstance();
+	percentFormat.setMinimumFractionDigits(1);
+}
+/**
+ * Overwrites.
+ */
+public java.awt.Component getTableCellRendererComponent(javax.swing.JTable table, Object value,
+						  boolean isSelected, boolean hasFocus, int row, int column) {
+    setComponentOrientation(java.awt.ComponentOrientation.RIGHT_TO_LEFT);
+	return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+}
+/**
+ * Overwrites.
+ */
+protected void setValue(Object value) {
+	setText((value == null) ? "" : percentFormat.format((Number)value));
+}
+}
