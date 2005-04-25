1608470cb42352c52efab37dfc6a011a45664a45
phirzel <phirzel@users.noreply.github.com>
2005-04-25 15:32:23 +0000

    new method #round()

--- src/ch/softenvironment/util/AmountFormat.java
+++ src/ch/softenvironment/util/AmountFormat.java
@@ -1,5 +1,7 @@
 package ch.softenvironment.util;
 
+import java.text.NumberFormat;
+
 /* 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -15,7 +17,7 @@ package ch.softenvironment.util;
 /**
  * Format a number to look like a financial value.
  * @author: Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.5 $ $Date: 2005-01-06 09:45:18 $
+ * @version $Revision: 1.6 $ $Date: 2005-04-25 15:32:23 $
  */
 public class AmountFormat /*extends java.text.NumberFormat*/ {
 
@@ -55,5 +57,34 @@ public static String toString(Number amount) {
 	} else {
 		return getAmountInstance().format(amount);
 	}
+}
+/**
+ * Round to 2nd decimal value.
+ * @param amount
+ * @return rounded amount
+ */
+public static double round(/*String iso4217Code,*/ double amount) {
+    if (amount == 0.0) {
+        return amount;
+    } else {
+        return ((double)(Math.round(amount * 100.0))) / 100.0;
+    }
+/*    try {
+	    if (StringUtils.isNullOrEmpty(iso4217Code)) {
+	        NumberFormat format = getAmountInstance();
+	        format.setGroupingUsed(false);
+	        return Double.parseDouble(format.format(amount));
+	    } else {
+	        NumberFormat format = DecimalFormat.getNumberInstance();
+	        format.setCurrency(Currency.getInstance(iso4217Code));
+	        String currency = format.format(amount);
+	        return Double.parseDouble(StringUtils.replace(currency, iso4217Code, ""));
+	    }
+    } catch(NumberFormatException e) {
+        Tracer.getInstance().developerWarning(AmountFormat.class, "round()", e.getLocalizedMessage());
+        // formatting suppressed
+    	return amount;
+    }
+*/
 }
 }
