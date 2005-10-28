62f1a554e9b48a7cca37d1a7747926e370ff5692
phirzel <phirzel@users.noreply.github.com>
2005-10-28 08:18:32 +0000

    new testcases

--- test/SeBaseTestSuite.java
+++ test/SeBaseTestSuite.java
@@ -3,6 +3,7 @@ import ch.softenvironment.client.test.ResourceManagerTestCase;
 import ch.softenvironment.math.test.MathTestSuite;
 import ch.softenvironment.util.test.AmountFormatTestCase;
 import ch.softenvironment.util.test.BeanReflectorTestCase;
+import ch.softenvironment.util.test.DOMUtilsTestCase;
 import ch.softenvironment.util.test.DateUtilsTestCase;
 import ch.softenvironment.util.test.NlsUtilsTestCase;
 import ch.softenvironment.util.test.ParserCSVTestCase;
@@ -12,7 +13,7 @@ import junit.framework.*;
 /**
  * Run this class for all <b>seBase</b> TestCases.
  * @author Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.2 $ $Date: 2005-09-22 15:18:55 $
+ * @version $Revision: 1.3 $ $Date: 2005-10-28 08:18:32 $
  */
 public class SeBaseTestSuite extends junit.framework.TestSuite {
 /**
@@ -48,17 +49,22 @@ public static void main(String[] args) {
  * @return junit.framework.Test
  */
 public static junit.framework.Test suite() {
-	TestSuite suite =  new TestSuite("Testing: ch.softenvironment.util.*");
+	TestSuite suite =  new TestSuite("Testing: seBase");
 
+    // *.util.test
+    suite.addTest(new TestSuite(AmountFormatTestCase.class));
 	suite.addTest(new TestSuite(BeanReflectorTestCase.class));
 	suite.addTest(new TestSuite(DateUtilsTestCase.class));
-	suite.addTest(new TestSuite(StringUtilsTestCase.class));
+    suite.addTest(new TestSuite(DOMUtilsTestCase.class));
 	suite.addTest(new TestSuite(NlsUtilsTestCase.class));
-	suite.addTest(new TestSuite(AmountFormatTestCase.class));
+	suite.addTest(new TestSuite(ParserCSVTestCase.class));
+    suite.addTest(new TestSuite(StringUtilsTestCase.class));
+    
+    // *.client.test
 	suite.addTest(new TestSuite(ApplicationOptionsTestCase.class));
 	suite.addTest(new TestSuite(ResourceManagerTestCase.class));
-	suite.addTest(new TestSuite(ParserCSVTestCase.class));
 	
+	// *.math.test
 	suite.addTest(MathTestSuite.suite());
 
 	/* in non VAJ environments:
