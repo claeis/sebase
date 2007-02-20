64611f92a8db337829cb8f44be0221550db768d4
phirzel <phirzel@users.noreply.github.com>
2007-02-20 12:35:58 +0000

    Feature: Tech-Panel added

--- src/ch/softenvironment/view/AboutDialog.java
+++ src/ch/softenvironment/view/AboutDialog.java
@@ -11,11 +11,11 @@ package ch.softenvironment.view;
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  */
- 
+import ch.softenvironment.client.ResourceManager;
 /**
  * Product Info Dialog.
- * @author: @author: Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.5 $ $Date: 2005-06-10 16:11:54 $
+ * @author Peter Hirzel <i>soft</i>Environment
+ * @version $Revision: 1.6 $ $Date: 2007-02-20 12:35:58 $
  */
 public class AboutDialog extends BaseDialog {
 	private javax.swing.JPanel ivjBaseDialogContentPane = null;
@@ -25,6 +25,22 @@ public class AboutDialog extends BaseDialog {
 	private javax.swing.JLabel ivjLblLicencedFor = null;
 	private javax.swing.JLabel ivjLblTitle = null;
 	private javax.swing.JTextArea ivjTxaLicence = null;
+	private javax.swing.JLabel ivjLblThankOSS = null;
+	private javax.swing.JPanel ivjPnlDeveloperInfo = null;
+	private PlatformInfoPanel ivjPnlPlatform = null;
+	private javax.swing.JPanel ivjPnlProduct = null;
+	private javax.swing.JTabbedPane ivjTbpMain = null;
+	private javax.swing.JTextArea ivjTxaOSS = null;
+/**
+ * Constructor
+ * @param owner Symbol
+ * @param modal Symbol
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+public AboutDialog(java.awt.Component owner, boolean modal) {
+	super(owner, modal);
+	initialize();
+}
 /**
  * AboutDialog constructor comment.
  * @param owner java.awt.Frame
@@ -36,10 +52,13 @@ public AboutDialog(java.awt.Frame owner, String application, String version, Str
 	initialize();
 
 	setTitle(getResourceString("DlgTitle") + " " + application);
-	getLblTitle().setText(application + " " + version);
+	getLblTitle().setText(/*application + " " +*/ version);
 	getLblCopyright().setText("Copyright (c) softEnvironment " + copyrightPeriod);
 	getTxaLicence().setText(licence);
 
+//TODO make OSS-list more flexible    
+    getTxaOSS().setText("- http://sourceforge.net/projects/umleditor" + "\n" +
+            "- http://sourceforge.net/projects/jomm");
 	show();
 }
 /**
@@ -62,12 +81,8 @@ private javax.swing.JPanel getBaseDialogContentPane() {
 		try {
 			ivjBaseDialogContentPane = new javax.swing.JPanel();
 			ivjBaseDialogContentPane.setName("BaseDialogContentPane");
-			ivjBaseDialogContentPane.setLayout(null);
-			getBaseDialogContentPane().add(getLblTitle(), getLblTitle().getName());
-			getBaseDialogContentPane().add(getLblLicencedFor(), getLblLicencedFor().getName());
-			getBaseDialogContentPane().add(getJPanel1(), getJPanel1().getName());
-			getBaseDialogContentPane().add(getLblCopyright(), getLblCopyright().getName());
-			getBaseDialogContentPane().add(getLblAbout(), getLblAbout().getName());
+			ivjBaseDialogContentPane.setLayout(new java.awt.BorderLayout());
+			getBaseDialogContentPane().add(getTbpMain(), "Center");
 			// user code begin {1}
 			// user code end
 		} catch (java.lang.Throwable ivjExc) {
@@ -90,7 +105,7 @@ private javax.swing.JPanel getJPanel1() {
 			ivjJPanel1.setName("JPanel1");
 			ivjJPanel1.setBorder(new javax.swing.border.EtchedBorder());
 			ivjJPanel1.setLayout(null);
-			ivjJPanel1.setBounds(64, 351, 383, 105);
+			ivjJPanel1.setBounds(62, 359, 383, 105);
 			getJPanel1().add(getTxaLicence(), getTxaLicence().getName());
 			// user code begin {1}
 			// user code end
@@ -113,7 +128,7 @@ private javax.swing.JLabel getLblAbout() {
 			ivjLblAbout = new javax.swing.JLabel();
 			ivjLblAbout.setName("LblAbout");
 			ivjLblAbout.setText("");
-			ivjLblAbout.setBounds(33, 73, 462, 240);
+			ivjLblAbout.setBounds(31, 81, 462, 240);
 			// user code begin {1}
 			ivjLblAbout.setIcon(ch.ehi.basics.i18n.ResourceBundle.getImageIcon(AboutDialog.class, "about.png"));
 			// user code end
@@ -136,7 +151,7 @@ private javax.swing.JLabel getLblCopyright() {
 			ivjLblCopyright = new javax.swing.JLabel();
 			ivjLblCopyright.setName("LblCopyright");
 			ivjLblCopyright.setText("Copyright (c) softEnvironment 2001-2003");
-			ivjLblCopyright.setBounds(127, 461, 280, 27);
+			ivjLblCopyright.setBounds(125, 469, 280, 27);
 			// user code begin {1}
 			ivjLblCopyright.setText("Copyright (c) softEnvironment 2001-2004");
 			// user code end
@@ -160,7 +175,7 @@ private javax.swing.JLabel getLblLicencedFor() {
 			ivjLblLicencedFor.setName("LblLicencedFor");
 			ivjLblLicencedFor.setFont(new java.awt.Font("Arial", 1, 14));
 			ivjLblLicencedFor.setText("Dieses Produkt ist lizenziert für:");
-			ivjLblLicencedFor.setBounds(64, 320, 285, 26);
+			ivjLblLicencedFor.setBounds(62, 328, 285, 26);
 			// user code begin {1}
 			ivjLblLicencedFor.setText(getResourceString("LblLicencedFor_text"));
 			// user code end
@@ -171,6 +186,27 @@ private javax.swing.JLabel getLblLicencedFor() {
 		}
 	}
 	return ivjLblLicencedFor;
+}
+/**
+ * Return the LblThankOSS property value.
+ * @return javax.swing.JLabel
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+private javax.swing.JLabel getLblThankOSS() {
+	if (ivjLblThankOSS == null) {
+		try {
+			ivjLblThankOSS = new javax.swing.JLabel();
+			ivjLblThankOSS.setName("LblThankOSS");
+			ivjLblThankOSS.setText("This product contains software developed by:");
+			// user code begin {1}
+			// user code end
+		} catch (java.lang.Throwable ivjExc) {
+			// user code begin {2}
+			// user code end
+			handleException(ivjExc);
+		}
+	}
+	return ivjLblThankOSS;
 }
 /**
  * Return the LblTitle property value.
@@ -184,7 +220,7 @@ private javax.swing.JLabel getLblTitle() {
 			ivjLblTitle.setName("LblTitle");
 			ivjLblTitle.setFont(new java.awt.Font("Arial", 1, 18));
 			ivjLblTitle.setText("Lepra-Projektadministration Vx.x.x");
-			ivjLblTitle.setBounds(15, 16, 497, 38);
+			ivjLblTitle.setBounds(13, 24, 497, 38);
 			// user code begin {1}
 			ivjLblTitle.setText("<your product name>");
 			// user code end
@@ -195,6 +231,117 @@ private javax.swing.JLabel getLblTitle() {
 		}
 	}
 	return ivjLblTitle;
+}
+/**
+ * Return the PnlDeveloperInfo property value.
+ * @return javax.swing.JPanel
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+private javax.swing.JPanel getPnlDeveloperInfo() {
+	if (ivjPnlDeveloperInfo == null) {
+		try {
+			ivjPnlDeveloperInfo = new javax.swing.JPanel();
+			ivjPnlDeveloperInfo.setName("PnlDeveloperInfo");
+			ivjPnlDeveloperInfo.setLayout(new java.awt.GridBagLayout());
+
+			java.awt.GridBagConstraints constraintsLblThankOSS = new java.awt.GridBagConstraints();
+			constraintsLblThankOSS.gridx = 1; constraintsLblThankOSS.gridy = 1;
+			constraintsLblThankOSS.anchor = java.awt.GridBagConstraints.NORTHWEST;
+			constraintsLblThankOSS.ipadx = 164;
+			constraintsLblThankOSS.insets = new java.awt.Insets(41, 28, 10, 80);
+			getPnlDeveloperInfo().add(getLblThankOSS(), constraintsLblThankOSS);
+
+			java.awt.GridBagConstraints constraintsTxaOSS = new java.awt.GridBagConstraints();
+			constraintsTxaOSS.gridx = 1; constraintsTxaOSS.gridy = 2;
+			constraintsTxaOSS.fill = java.awt.GridBagConstraints.BOTH;
+			constraintsTxaOSS.anchor = java.awt.GridBagConstraints.NORTHWEST;
+			constraintsTxaOSS.weightx = 1.0;
+			constraintsTxaOSS.weighty = 1.0;
+			constraintsTxaOSS.ipadx = 475;
+			constraintsTxaOSS.ipady = 389;
+			constraintsTxaOSS.insets = new java.awt.Insets(10, 25, 47, 29);
+			getPnlDeveloperInfo().add(getTxaOSS(), constraintsTxaOSS);
+			// user code begin {1}
+			// user code end
+		} catch (java.lang.Throwable ivjExc) {
+			// user code begin {2}
+			// user code end
+			handleException(ivjExc);
+		}
+	}
+	return ivjPnlDeveloperInfo;
+}
+/**
+ * Return the PnlPlatform property value.
+ * @return ch.softenvironment.view.PlatformInfoPanel
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+private PlatformInfoPanel getPnlPlatform() {
+	if (ivjPnlPlatform == null) {
+		try {
+			ivjPnlPlatform = new ch.softenvironment.view.PlatformInfoPanel();
+			ivjPnlPlatform.setName("PnlPlatform");
+			// user code begin {1}
+			// user code end
+		} catch (java.lang.Throwable ivjExc) {
+			// user code begin {2}
+			// user code end
+			handleException(ivjExc);
+		}
+	}
+	return ivjPnlPlatform;
+}
+/**
+ * Return the PnlProduct property value.
+ * @return javax.swing.JPanel
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+private javax.swing.JPanel getPnlProduct() {
+	if (ivjPnlProduct == null) {
+		try {
+			ivjPnlProduct = new javax.swing.JPanel();
+			ivjPnlProduct.setName("PnlProduct");
+			ivjPnlProduct.setLayout(null);
+			getPnlProduct().add(getLblTitle(), getLblTitle().getName());
+			getPnlProduct().add(getLblLicencedFor(), getLblLicencedFor().getName());
+			getPnlProduct().add(getJPanel1(), getJPanel1().getName());
+			getPnlProduct().add(getLblCopyright(), getLblCopyright().getName());
+			getPnlProduct().add(getLblAbout(), getLblAbout().getName());
+			// user code begin {1}
+			// user code end
+		} catch (java.lang.Throwable ivjExc) {
+			// user code begin {2}
+			// user code end
+			handleException(ivjExc);
+		}
+	}
+	return ivjPnlProduct;
+}
+/**
+ * Return the TbpMain property value.
+ * @return javax.swing.JTabbedPane
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+private javax.swing.JTabbedPane getTbpMain() {
+	if (ivjTbpMain == null) {
+		try {
+			ivjTbpMain = new javax.swing.JTabbedPane();
+			ivjTbpMain.setName("TbpMain");
+			ivjTbpMain.insertTab("Produkt-Info", null, getPnlProduct(), null, 0);
+			ivjTbpMain.insertTab("PnlPlatform", null, getPnlPlatform(), null, 1);
+			ivjTbpMain.insertTab("Entwickler-Info", null, getPnlDeveloperInfo(), null, 2);
+			// user code begin {1}
+			ivjTbpMain.setTitleAt(0, ResourceManager.getResource(AboutDialog.class, "PnlProductInfo_text"));
+			ivjTbpMain.setTitleAt(1, ResourceManager.getResource(AboutDialog.class, "PnlTechnicalInfo_text"));
+			ivjTbpMain.setTitleAt(2, ResourceManager.getResource(AboutDialog.class, "PnlDeveloperInfo_text"));
+			// user code end
+		} catch (java.lang.Throwable ivjExc) {
+			// user code begin {2}
+			// user code end
+			handleException(ivjExc);
+		}
+	}
+	return ivjTbpMain;
 }
 /**
  * Return the TxaLicence property value.
@@ -218,6 +365,27 @@ private javax.swing.JTextArea getTxaLicence() {
 		}
 	}
 	return ivjTxaLicence;
+}
+/**
+ * Return the TxaOSS property value.
+ * @return javax.swing.JTextArea
+ */
+/* WARNING: THIS METHOD WILL BE REGENERATED. */
+private javax.swing.JTextArea getTxaOSS() {
+	if (ivjTxaOSS == null) {
+		try {
+			ivjTxaOSS = new javax.swing.JTextArea();
+			ivjTxaOSS.setName("TxaOSS");
+			// user code begin {1}
+            ivjTxaOSS.setEditable(false);
+			// user code end
+		} catch (java.lang.Throwable ivjExc) {
+			// user code begin {2}
+			// user code end
+			handleException(ivjExc);
+		}
+	}
+	return ivjTxaOSS;
 }
 /**
  * Called whenever the part throws an exception.
@@ -244,6 +412,7 @@ private void initialize() {
 	}
 	// user code begin {2}
 	setTitle(getResourceString("DlgTitle"));
+    setSize(534, 567);
 	// user code end
 }
 }
