51b705329f35c518e792c1b91b318b0f41700ca9
phirzel <phirzel@users.noreply.github.com>
2007-03-06 13:00:27 +0000

    new method #sort()

--- src/ch/softenvironment/util/ListUtils.java
+++ src/ch/softenvironment/util/ListUtils.java
@@ -12,13 +12,94 @@ package ch.softenvironment.util;
  * Lesser General Public License for more details.
  */
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Locale;
+
 /**
  * Provides some often used features in using java.util.List.
  * @author Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.3 $ $Date: 2007-02-20 12:49:15 $
+ * @version $Revision: 1.4 $ $Date: 2007-03-06 13:00:27 $
  */
 public abstract class ListUtils {
+    /**
+     * Comparator for sorting Object's by a defined property.
+     */
+    private static class ObjectPropertyComparator implements java.util.Comparator {
+        private Evaluator evaluator = null;
+        private String property = null;
+        private java.text.Collator collator = null;
+            
+        /**
+         * @param property Attribute of Objects in list to sort by.
+         */
+        protected ObjectPropertyComparator(Evaluator evaluator, final String property) {
+            super();
+        
+            if (evaluator == null) {
+                throw new IllegalArgumentException("evaluator must not be null");
+            }
+            if (property == null) {
+                throw new IllegalArgumentException("property must not be null");
+            }
+            
+            this.evaluator = evaluator;
+            this.property = property;
+            
+            collator = java.text.Collator.getInstance(Locale.getDefault());
+        }
+        /**
+         * @see java.util.Comparator
+         * @param o1 ValueWrapper
+         * @param o1 ValueWrapper
+         */
+        public int compare(java.lang.Object o1, java.lang.Object o2) {
+            try {
+                if (o1 == null) {
+                    if (o2 == null) {
+                        return 0;
+                    } else {
+                        return -1; // make sure null-element comes first
+                    }
+                } else if (o2 == null) {
+                    return 1;
+                } else {
+                    Object v1 = evaluator.evaluate(o1, property);
+                    Object v2 = evaluator.evaluate(o2, property);
+                    if (v1 == null) {
+                        if (v2 == null) {
+                            return 0;
+                        } else {
+                            return -1; // make sure null-element comes first
+                        }
+                    } else if (v2 == null) {
+                        return 1;
+                    } else {
+                        if ((v1 instanceof String) && (v2 instanceof String)) {
+                            String s1 = (v1 == null ? "" : v1.toString());                    
+                            String s2 = (v1 == null ? "" : v2.toString());
+                            return collator.compare(s1, s2);
+                        } else {
+                            if ((v1 instanceof Long) && (v2 instanceof Long)) {
+                                return ((Long)o1).compareTo((Long)o2);
+                            } if ((v1 instanceof Integer) && (v2 instanceof Integer)) {
+                                return ((Integer)o1).compareTo((Integer)o2);
+                            } else if ((v1 instanceof Double) && (v2 instanceof Double)) {
+                                return ((Double)v1).compareTo((Double)v2);
+                            } else {
+//TODO evtl. compare is not correct for any other type                            
+                                return v1.toString().compareTo(v2.toString());
+                            }
+                        }
+                    }
+                }
+            } catch(Throwable e) {
+                ch.softenvironment.util.Tracer.getInstance().runtimeWarning("sort failed: " + e.getLocalizedMessage());
+                return 0; // as is
+            }
+        }
+    }
+
 /**
  * Return an ArrayList containing the given item.
  * @return java.util.ArrayList
@@ -27,6 +108,33 @@ public static java.util.List createList(Object item) {
 	java.util.List list = new java.util.ArrayList(1);
 	list.add(item);
 	return list;
+}
+/**
+ * Sort the given items according to their objects property
+ * determined by evaluator.
+ * @param items list to sorted
+ * @param evaluator
+ * @param property
+ * @return cloned and sorted list
+ */
+public static java.util.List sort(java.util.List items, Evaluator evaluator, final String property) {
+    if ((items == null) || (items.size() == 0)) {
+        return items;
+    }
+    if (StringUtils.isNullOrEmpty(property)) {
+        throw new IllegalArgumentException("property must not be null");
+    }
+    
+    // clone the list -> do not influence original items-list
+    java.util.List list = new ArrayList(items.size());
+    Iterator it = items.iterator();
+    while (it.hasNext()) {
+        list.add(it.next());
+    }
+    
+    // sort list
+    java.util.Collections.sort(list, new ObjectPropertyComparator(evaluator, property));
+    return list;
 }
 /**
  * Convert elements of the list to a String expression by given method,
