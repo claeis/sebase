d0bda0a8d2588ae845fae83fd57ba109cc303780
phirzel <phirzel@users.noreply.github.com>
2007-05-30 07:40:17 +0000

    Bug fix: Sorting API changed

--- src/ch/softenvironment/view/swingext/JComboBoxUtility.java
+++ src/ch/softenvironment/view/swingext/JComboBoxUtility.java
@@ -32,14 +32,17 @@ import ch.softenvironment.util.ListUtils;
  * JComboBox utility to display sorted objects in items by its
  * property determined by evaluator.
  * @author Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.1 $ $Date: 2007-03-06 12:56:19 $
+ * @version $Revision: 1.2 $ $Date: 2007-05-30 07:40:17 $
  */
 public class JComboBoxUtility {
+    public static final int SORT_KEEP_ORDER = 100;
+    public static final int SORT_ASCENDING = 101;
+    //public static final int SORT_DESCENDING = 102;
     /**
      * Default generic generic Evaluator assuming getter-method.
      * @see BeanReflector
      */
-    private static class BeanEvaluator implements Evaluator {
+    protected static class BeanEvaluator implements Evaluator {
         public Object evaluate(Object owner, final String property) {
             try {
                 if (owner != null) {
@@ -55,7 +58,7 @@ public class JComboBoxUtility {
     /**
      * Display the value of a given public property of the represented object.
      */
-    private static class ObjectComboBoxRenderer extends BasicComboBoxRenderer {
+    protected static class ObjectComboBoxRenderer extends BasicComboBoxRenderer {
         private Evaluator evaluator = null;
         private String property = null;
         /**
@@ -63,7 +66,7 @@ public class JComboBoxUtility {
          * @param evaluator
          * @param property
          */
-        private ObjectComboBoxRenderer(Evaluator evaluator, final String property) {
+        protected ObjectComboBoxRenderer(Evaluator evaluator, final String property) {
             super();
             this.evaluator = evaluator;
             this.property = property;
@@ -96,21 +99,29 @@ public class JComboBoxUtility {
      * Extended DefaultComboBoxModel to sort ascendingly by displayable value of
      * given by an object's property.
      */
-    private static class SortedComboBoxModel extends DefaultComboBoxModel {
+    protected static class SortedComboBoxModel extends DefaultComboBoxModel {
         /**
          * Initialize the list.
          * @param items
          * @param addNullElement add a null-element as very first element
          * @param evaluator
          * @param property
+         * @param sort
          */
-        private SortedComboBoxModel(java.util.List items, Evaluator evaluator, final String property, boolean addNullElement) {
+        protected SortedComboBoxModel(java.util.List items, Evaluator evaluator, final String property, boolean addNullElement, final int sort) {
             super();        
             if (addNullElement) {
                 // insert an empty object as very first element
                 addElement(null);
             }
-            java.util.Iterator iterator = ListUtils.sort(items, evaluator, property).iterator();
+            
+            java.util.Iterator iterator = null;
+            if (sort == SORT_ASCENDING) {
+                iterator = ListUtils.sort(items, evaluator, property).iterator();
+            } else {
+                // keep given order
+                iterator = items.iterator();
+            }
             while (iterator.hasNext()) {
                 addElement(iterator.next());
             }
@@ -129,21 +140,30 @@ public class JComboBoxUtility {
         initComboBox(comboBox, items, property, addNullElement, new BeanEvaluator());    
     }
     /**
-     * Setup a JComboBox with a sorted list of items and a
+     * Sort items ascendingly by default.
+     * @see #initComboBox(JComboBox, java.util.List, String, boolean, Evaluator, boolean)
+     */
+    public static void initComboBox(JComboBox comboBox, java.util.List items, final String property, boolean addNullElement, Evaluator evaluator) {
+        initComboBox(comboBox, items, property, addNullElement, evaluator, SORT_ASCENDING);
+    }
+    /**
+     * Setup a JComboBox with an optionally sorted list of items and a
      * renderer which shows value of each object#property of
      * each element in items.
      * @param comboBox
      * @param items
      * @param property contained in each object within items
-     * @param addNullItem whether items list shall have a very first null-Element
+     * @param addNullElement whether items list shall have a very first null-Element
      * @param evaluator uses default BeanEvaluator if null
+     * @param sort sort text items according to SORT_*
      */
-    public static void initComboBox(JComboBox comboBox, java.util.List items, final String property, boolean addNullItem, Evaluator evaluator) {
+    public static void initComboBox(JComboBox comboBox, java.util.List items, final String property, boolean addNullElement, Evaluator evaluator, final int sort) {
         Evaluator eval = evaluator;
         if (eval == null) {
+            // set the standard evaluator #get*()
             eval = new BeanEvaluator();
         }
         comboBox.setRenderer(new ObjectComboBoxRenderer(eval, property));
-        comboBox.setModel(new SortedComboBoxModel(items, eval, property, addNullItem));
+        comboBox.setModel(new SortedComboBoxModel(items, eval, property, addNullElement, sort));
     }    
 }
