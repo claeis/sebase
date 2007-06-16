38ae46438ea164e52f1f25e94df5ef8de370c7b4
phirzel <phirzel@users.noreply.github.com>
2007-06-16 14:00:18 +0000

    Test enhanced

--- test/ch/softenvironment/util/test/AmountFormatTestCase.java
+++ test/ch/softenvironment/util/test/AmountFormatTestCase.java
@@ -1,12 +1,22 @@
 package ch.softenvironment.util.test;
 
-import java.text.NumberFormat;
+/* 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
 import java.util.Locale;
 
 import ch.softenvironment.util.AmountFormat;
 /**
  * @author Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.2 $ $Date: 2007-02-20 12:57:46 $
+ * @version $Revision: 1.3 $ $Date: 2007-06-16 14:00:18 $
  */
 public class AmountFormatTestCase extends junit.framework.TestCase {
 /**
@@ -18,17 +28,35 @@ public AmountFormatTestCase(String arg1) {
 }
 public void testFormat() {
 	Locale.setDefault(new Locale("de", "CH"));
-	assertTrue("toString(double)", AmountFormat.toString(1343492.3422).equals("1'343'492.34"));
-	assertTrue("toString(Double)", AmountFormat.toString(new Double(1343492.3422)).equals("1'343'492.34"));
-//	assertTrue("toString(long)", AmountFormat.toString((long)1343492).equals("1'343'492.00"));
-	assertTrue("toString(Long)", AmountFormat.toString(new Long(1343492)).equals("1'343'492.00"));
-
-	assertTrue("toString(null)", AmountFormat.toString(null).equals(""));
-//	assertTrue("toString(Double, 3, 5)", AmountFormat.toString(new Double(1343492.3422567), 3, 5).equals("1'343'492.34226"));
-//	assertTrue("toString(Double,3 ,3)", AmountFormat.toString(new Double(1343492.3), 3, 3).equals("1'343'492.300"));
-
-	NumberFormat af = AmountFormat.getAmountInstance();
-//	assertTrue("format(Double)", af.format(null).equals(""));
+	
+    java.text.NumberFormat af = AmountFormat.getAmountInstance(/*default*/);
+//	assertTrue("format(null) => Exception", af.format(null).equals(""));
 	assertTrue("format(Double)", af.format(new Double(12345.563)).equals("12'345.56"));
+    assertTrue("toString(double)", af.format(1343492.3422).equals("1'343'492.34"));
+    assertTrue("toString(Double)", af.format(new Double(1343492.3422)).equals("1'343'492.34"));
+    assertTrue("toString(long)", af.format((long)1343492).equals("1'343'492.00"));
+    assertTrue("toString(Long)", af.format(new Long(1343492)).equals("1'343'492.00"));
+
+    af = AmountFormat.getAmountInstance(new Locale("de", "CH"));
+//  assertTrue("format(null) => Exception", af.format(null).equals(""));
+    assertTrue("format(Double)", af.format(new Double(12345.563)).equals("12'345.56"));  
+    assertTrue("toString(double)", af.format(1343492.3422).equals("1'343'492.34"));
+    assertTrue("toString(Double)", af.format(new Double(1343492.3422)).equals("1'343'492.34"));
+    assertTrue("toString(long)", af.format((long)1343492).equals("1'343'492.00"));
+    assertTrue("toString(Long)", af.format(new Long(1343492)).equals("1'343'492.00"));
+
+    
+    af = AmountFormat.getAmountInstance(new Locale("en", "US"));
+//  assertTrue("format(null) => Exception", af.format(null).equals(""));
+    assertTrue("format(Double)", af.format(new Double(12345.563)).equals("12,345.56"));  
+    assertTrue("toString(double)", af.format(1343492.3422).equals("1,343,492.34"));
+    assertTrue("toString(Double)", af.format(new Double(1343492.3422)).equals("1,343,492.34"));
+    assertTrue("toString(long)", af.format((long)1343492).equals("1,343,492.00"));
+    assertTrue("toString(Long)", af.format(new Long(1343492)).equals("1,343,492.00"));
+
+    assertTrue("toString(null)", AmountFormat.toString(null).equals(""));
+//  assertTrue("toString(Double, 3, 5)", AmountFormat.toString(new Double(1343492.3422567), 3, 5).equals("1'343'492.34226"));
+//  assertTrue("toString(Double,3 ,3)", AmountFormat.toString(new Double(1343492.3), 3, 3).equals("1'343'492.300"));
+
 }
 }
