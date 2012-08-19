fc7bc425f0208833ada472f4cd6da32c4673430a
phirzel <phirzel@users.noreply.github.com>
2012-08-19 09:45:49 +0000

    Javadoc, source-format

--- src/ch/softenvironment/view/tree/TransferableTreeNode.java
+++ src/ch/softenvironment/view/tree/TransferableTreeNode.java
@@ -1,4 +1,5 @@
 package ch.softenvironment.view.tree;
+
 /*
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,34 +17,36 @@ package ch.softenvironment.view.tree;
  */
 import java.awt.datatransfer.*;
 import javax.swing.tree.*;
+
 /**
- * Tool for Mouse-Drag withing a JTree.
- * @author Peter Hirzel <i>soft</i>Environment
- * @version $Revision: 1.2 $ $Date: 2005-06-30 07:29:01 $
+ * Tool for Mouse-Drag within a JTree.
+ * 
+ * @author Peter Hirzel, softEnvironment GmbH
  */
 class TransferableTreeNode implements Transferable {
-  protected static final DataFlavor TREE_PATH_FLAVOR = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType,
-							     									"Tree Path");
-  private DataFlavor flavors[] = { TREE_PATH_FLAVOR };
-  private TreePath path = null;
+	protected static final DataFlavor TREE_PATH_FLAVOR = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType, "Tree Path");
+	private DataFlavor flavors[] = { TREE_PATH_FLAVOR };
+	private TreePath path = null;
 
-  public TransferableTreeNode(TreePath path) {
-      super();
-      this.path = path;
-  }
-  public synchronized Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException {
-    if (isDataFlavorSupported(flavor)) {
-      return path;
-    } else {
-      throw new UnsupportedFlavorException(flavor);
-    }
-  }
-  public synchronized DataFlavor[] getTransferDataFlavors() {
-    return flavors;
-  }
-  public boolean isDataFlavorSupported(DataFlavor flavor) {
-    return (flavor != null) && 
-    		flavor.getMimeType().equals(TREE_PATH_FLAVOR.getMimeType()) &&
-    		flavor.getHumanPresentableName().equals(TREE_PATH_FLAVOR.getHumanPresentableName());
-  }
+	public TransferableTreeNode(TreePath path) {
+		super();
+		this.path = path;
+	}
+
+	public synchronized Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException {
+		if (isDataFlavorSupported(flavor)) {
+			return path;
+		} else {
+			throw new UnsupportedFlavorException(flavor);
+		}
+	}
+
+	public synchronized DataFlavor[] getTransferDataFlavors() {
+		return flavors;
+	}
+
+	public boolean isDataFlavorSupported(DataFlavor flavor) {
+		return (flavor != null) && flavor.getMimeType().equals(TREE_PATH_FLAVOR.getMimeType())
+				&& flavor.getHumanPresentableName().equals(TREE_PATH_FLAVOR.getHumanPresentableName());
+	}
 }
