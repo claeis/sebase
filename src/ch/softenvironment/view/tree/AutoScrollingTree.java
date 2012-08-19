68a758f052b8bdd0c8605acdff4a3bb5bf7b7efb
phirzel <phirzel@users.noreply.github.com>
2012-08-19 09:38:07 +0000

    Rollback latest refactorings => Drag'n'Drop reactivated!

--- src/ch/softenvironment/view/tree/AutoScrollingTree.java
+++ src/ch/softenvironment/view/tree/AutoScrollingTree.java
@@ -25,57 +25,60 @@ import java.awt.dnd.DnDConstants;
  */
 @SuppressWarnings("serial")
 public class AutoScrollingTree extends javax.swing.JTree implements java.awt.dnd.Autoscroll {
-    private int margin = 12;
-    //private TreeDragSource ds = null;
-    //private TreeDropTarget dt = null;
+	private int margin = 12;
+	@SuppressWarnings("unused")
+	private TreeDragSource ds = null;
+	@SuppressWarnings("unused")
+	private TreeDropTarget dt = null;
 
-    /**
-     * Create an AutoScrolling JTree with "drag and drop" support.
-     * 
-     * @param dndType
-     *            according to java.awt.dnd.DnDConstants
-     */
-    public AutoScrollingTree(final int dndType) {
-	super();
-	// drag/drop support
-	if (dndType == DnDConstants.ACTION_MOVE) {
-	    //ds = new TreeDragSource(this, dndType);
-	    //dt = new TreeDropTarget(this);
-	} else {
-	    // TODO implement other dnd-types
+	/**
+	 * Create an AutoScrolling JTree with "drag and drop" support.
+	 * 
+	 * @param dndType
+	 *            according to java.awt.dnd.DnDConstants
+	 */
+	public AutoScrollingTree(final int dndType) {
+		super();
+		// drag/drop support
+		if (dndType == DnDConstants.ACTION_MOVE) {
+			ds = new TreeDragSource(this, dndType);
+			dt = new TreeDropTarget(this);
+		} else {
+			// TODO implement other dnd-types
+		}
 	}
-    }
 
-    /**
-     * Ok, we've been told to scroll because the mouse cursor is in our scroll
-     * zone.
-     */
-    public void autoscroll(java.awt.Point p) {
-	// Figure out which row we're on.
-	int realrow = getClosestRowForLocation(p.x, p.y);
-	java.awt.Rectangle outer = getBounds();
-	// Now decide if the row is at the top of the screen or at the
-	// bottom. We do this to make the previous row (or the next
-	// row) visible as appropriate. If we're at the absolute top or
-	// bottom, just return the first or last row respectively.
-	realrow = (p.y + outer.y <= margin ? realrow - 1 <= 0 ? 0 : realrow - 1 : realrow + 1 >= getRowCount() - 1 ? getRowCount() - 1 : realrow + 1);
-	scrollRowToVisible(realrow);
-    }
+	/**
+	 * Ok, we've been told to scroll because the mouse cursor is in our scroll
+	 * zone.
+	 */
+	public void autoscroll(java.awt.Point p) {
+		// Figure out which row we're on.
+		int realrow = getClosestRowForLocation(p.x, p.y);
+		java.awt.Rectangle outer = getBounds();
+		// Now decide if the row is at the top of the screen or at the
+		// bottom. We do this to make the previous row (or the next
+		// row) visible as appropriate. If we're at the absolute top or
+		// bottom, just return the first or last row respectively.
+		realrow = (p.y + outer.y <= margin ? realrow - 1 <= 0 ? 0 : realrow - 1 : realrow + 1 >= getRowCount() - 1 ? getRowCount() - 1 : realrow + 1);
+		scrollRowToVisible(realrow);
+	}
 
-    /**
-     * Calculate the insets for the *JTRee*, not the viewport the tree is in.
-     * This makes it a bit messy.
-     */
-    public java.awt.Insets getAutoscrollInsets() {
-	java.awt.Rectangle outer = getBounds();
-	java.awt.Rectangle inner = getParent().getBounds();
-	return new java.awt.Insets(inner.y - outer.y + margin, inner.x - outer.x + margin, outer.height - inner.height - inner.y + outer.y + margin, outer.width - inner.width - inner.x + outer.x + margin);
-    }
+	/**
+	 * Calculate the insets for the *JTRee*, not the viewport the tree is in.
+	 * This makes it a bit messy.
+	 */
+	public java.awt.Insets getAutoscrollInsets() {
+		java.awt.Rectangle outer = getBounds();
+		java.awt.Rectangle inner = getParent().getBounds();
+		return new java.awt.Insets(inner.y - outer.y + margin, inner.x - outer.x + margin, outer.height - inner.height - inner.y + outer.y + margin,
+				outer.width - inner.width - inner.x + outer.x + margin);
+	}
 
-    /**
-     * Convenience Method.
-     */
-    protected TreeNodeUtility getUtility() {
-	return ((NavigationTreeModel)getModel()).getUtility();
-    }
+	/**
+	 * Convenience Method.
+	 */
+	protected TreeNodeUtility getUtility() {
+		return ((NavigationTreeModel)getModel()).getUtility();
+	}
 }
