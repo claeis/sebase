b96e1d26da739be99d3aa1eeeb20933792df62a5
phirzel <phirzel@users.noreply.github.com>
2013-08-12 20:01:13 +0000

    MDE support

---
+++ src/ch/softenvironment/persistence/types/Code.java
@@ -0,0 +1,10 @@
+package ch.softenvironment.persistence.types;
+
+/**
+ * Code type (like Branch, Country,..).
+ * @author Peter Hirzel, softEnvironment GmbH
+ */
+public interface Code {
+    //TODO StringNls getName();
+    java.lang.String /*len=15*/ getShortName();
+}
---
+++ src/ch/softenvironment/persistence/types/StringNls.java
@@ -0,0 +1,52 @@
+package ch.softenvironment.persistence.types;
+
+import java.io.Serializable;
+
+import ch.softenvironment.util.DeveloperException;
+
+/**
+ * Translatable String type (i18n).
+ * @author Peter Hirzel, softEnvironment GmbH
+ */
+public class StringNls implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    
+    private Object owner = null;
+    /**
+     * The owning instance having this StringNls property as an attribute.
+     * @return
+     */
+    public Object getOwner() {
+            return owner;
+        }
+    public void setOwner(Object owner) {
+            if (owner == null) {
+                throw new IllegalArgumentException("owner");
+            }
+            if (this.owner != null) {
+                throw new DeveloperException("StringNls is already bound to owner");
+            }
+            this.owner = owner;
+        }
+    
+    private String propertyName = "";
+    /**
+     * The name of the owning instance property to point on StringNls value.
+     * @see getOwner
+     * @return
+     */
+    public String getPropertyName() {
+	return this.propertyName;
+    }
+   
+    public void setPropertyName(String propertyName) {
+	if (propertyName == null) {
+	    throw new IllegalArgumentException("propertyName");
+	}
+	if (this.propertyName == null) {
+	    throw new DeveloperException("propertyName is already set");
+	}
+	this.propertyName = propertyName;
+    }
+}
